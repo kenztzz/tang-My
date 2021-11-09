fd5a72c901c89d048635e0f73b965585a5b58bce
kenztzz <93968039+kenztzz@users.noreply.github.com>
2021-11-09 14:09:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     introTitle: 'Babie à!',
-    introDesc: `Trái đất vốn dĩ lạ thường
-    mà sao a cứ 1 mực nhớ eim
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao a cứ 1 mực nhớ e
     
-    A có 1 siêu năng lực
-    đó là nhắm mắt lại là có thể thấy e:'>>`,
-    btnIntro: 'iu qué nèee :'>',
+    S có 1 siêu năng lực
+    đó là nhắm mắt lại là có thể thấy e`,
+    btnIntro: 'hihii',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'thích qué rùi nèee <33',
-    btnNo: 'A với nhà không giống nhâu đâu :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh vậy <3',
-    btnReply: 'nói nhỏ a nghe đây <3',
+    btnYes: 'thích quá rùi nèe <33',
+    btnNo: 'a với ngôi nhà không giống nhau đâu!! :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'nói nhỏ cho a nghe đii <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'i know 🥰. Luv u 3000 lunn 😘😘',
-    messDesc: 'Tối nay 7h anh rước nàng tiên hạ phàm nhaa ',
+    mess: 'Anh biết mà 🥰. Yêu em 3 ngàn nuôn 😘😘',
+    messDesc: 'Tối nay 7h a đưa tiểu tiên hạ phàm nhaasaa hihii',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com/cuulan.1612' //link mess của các bạn. VD: https://m.me/nam.nodemy
-} 
+}
