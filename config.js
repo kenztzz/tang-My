d6c8cc0f3a61f34dfbd74ac8abb2097b45e60a08
kenztzz <93968039+kenztzz@users.noreply.github.com>
2021-11-09 13:48:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
     introTitle: 'Babie à!',
     introDesc: `Trái đất vốn dĩ lạ thường
-    mà sao anh cứ 1 mực nhớ eim
+    mà sao a cứ 1 mực nhớ eim
     
     A có 1 siêu năng lực
     đó là nhắm mắt lại là có thể thấy e:'>>`,
-    btnIntro: 'hihi',
+    btnIntro: 'iu qué nefee :'>',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'thích qué rùi nèee <33',
@@ -14,7 +14,7 @@ const CONFIG = {
     btnReply: 'nói nhỏ a nghe đây <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'i know 🥰. Luv u 3000 lunn 😘😘',
-    messDesc: 'Tối nay 7h anh rước nàng tiên xuống nhân gian nhaa ',
+    messDesc: 'Tối nay 7h anh rước nàng tiên hạ phàm nhaa ',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com/cuulan.1612' //link mess của các bạn. VD: https://m.me/nam.nodemy
 } 
